582b35627674a445bc3054971617c0d438182003
Sergey Logachev <serg@baitek.org>
2019-12-11 14:43:05 +0300

    simplified condition

--- src/GraphQl/Type/TypeConverter.php
+++ src/GraphQl/Type/TypeConverter.php
@@ -61,8 +61,7 @@ final class TypeConverter implements TypeConverterInterface
             case Type::BUILTIN_TYPE_STRING:
                 return GraphQLType::string();
             case Type::BUILTIN_TYPE_ARRAY:
-                if ($type->getCollectionValueType() &&
-                    (null !== $collectionValue = $type->getCollectionValueType()) &&
+                if ((null !== $collectionValue = $type->getCollectionValueType()) &&
                     (Type::BUILTIN_TYPE_OBJECT === $collectionValue->getBuiltinType())
                 ) {
                     return $this->getResourceType($collectionValue, $input, $queryName, $mutationName, $depth);
